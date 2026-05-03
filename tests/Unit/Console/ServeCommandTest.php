cff4a15365c318c137e7959936c631b3560747ea
Alexander Makarov <sam@rmcreative.ru>
2026-05-03 10:53:05 +0300

    Ignore disconnected packaged serve clients

--- tests/Unit/Console/ServeCommandTest.php
+++ tests/Unit/Console/ServeCommandTest.php
@@ -8,6 +8,7 @@ use App\Console\ServeCommand;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 use ReflectionMethod;
+use Socket;
 use Symfony\Component\Console\Tester\CommandTester;
 use Yiisoft\Yii\Console\ExitCode;
 use Yiisoft\Yii\Runner\Http\HttpApplicationRunner;
@@ -37,5 +38,40 @@ final class ServeCommandTest extends TestCase
         self::assertInstanceOf(HttpApplicationRunner::class, $firstRunner);
         self::assertInstanceOf(HttpApplicationRunner::class, $secondRunner);
         self::assertNotSame($firstRunner, $secondRunner);
+    }
+
+    #[Test]
+    public function packagedServeIgnoresDisconnectedClientSocketWrites(): void
+    {
+        $sockets = [];
+        self::assertTrue(socket_create_pair(AF_UNIX, SOCK_STREAM, 0, $sockets));
+        self::assertCount(2, $sockets);
+
+        $serverSocket = $sockets[0];
+        $clientSocket = $sockets[1];
+        self::assertInstanceOf(Socket::class, $serverSocket);
+        self::assertInstanceOf(Socket::class, $clientSocket);
+
+        socket_close($clientSocket);
+
+        $command = new ServeCommand(packaged: true);
+        $method = new ReflectionMethod($command, 'writeSocket');
+
+        $warnings = [];
+        set_error_handler(
+            static function (int $severity, string $message) use (&$warnings): bool {
+                $warnings[] = [$severity, $message];
+
+                return true;
+            },
+        );
+
+        try {
+            self::assertFalse($method->invoke($command, $serverSocket, "HTTP/1.1 200 OK\r\n\r\n"));
+            self::assertSame([], $warnings);
+        } finally {
+            restore_error_handler();
+            socket_close($serverSocket);
+        }
     }
 }
