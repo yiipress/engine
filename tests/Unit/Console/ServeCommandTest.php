92420291e3af691be9cdc24490caea0a318cce2e
Alexander Makarov <sam@rmcreative.ru>
2026-05-03 10:44:37 +0300

    Reset packaged serve state per request

--- tests/Unit/Console/ServeCommandTest.php
+++ tests/Unit/Console/ServeCommandTest.php
@@ -7,8 +7,10 @@ namespace App\Tests\Unit\Console;
 use App\Console\ServeCommand;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
+use ReflectionMethod;
 use Symfony\Component\Console\Tester\CommandTester;
 use Yiisoft\Yii\Console\ExitCode;
+use Yiisoft\Yii\Runner\Http\HttpApplicationRunner;
 
 final class ServeCommandTest extends TestCase
 {
@@ -21,5 +23,19 @@ final class ServeCommandTest extends TestCase
 
         self::assertSame(ExitCode::OK, $exitCode);
         self::assertStringContainsString('embedded PHAR application', $tester->getDisplay());
+    }
+
+    #[Test]
+    public function packagedServeCreatesFreshHttpRunnerPerRequest(): void
+    {
+        $command = new ServeCommand(packaged: true);
+        $method = new ReflectionMethod($command, 'createHttpRunner');
+
+        $firstRunner = $method->invoke($command);
+        $secondRunner = $method->invoke($command);
+
+        self::assertInstanceOf(HttpApplicationRunner::class, $firstRunner);
+        self::assertInstanceOf(HttpApplicationRunner::class, $secondRunner);
+        self::assertNotSame($firstRunner, $secondRunner);
     }
 }
