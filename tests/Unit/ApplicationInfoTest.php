713043b5dcaedf2682bf613283c6e1f634268ece
Alexander Makarov <sam@rmcreative.ru>
2026-05-03 16:20:58 +0300

    Set YiiPress console application version

---
+++ tests/Unit/ApplicationInfoTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit;
+
+use App\ApplicationInfo;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+final class ApplicationInfoTest extends TestCase
+{
+    #[Test]
+    public function versionIsUserFacingReleaseVersion(): void
+    {
+        self::assertSame('YiiPress', ApplicationInfo::NAME);
+        self::assertMatchesRegularExpression('/^\d+\.\d+\.\d+/', ApplicationInfo::version());
+        self::assertStringNotContainsString('no-version-set', ApplicationInfo::version());
+    }
+}
